35508f0a57ed2d61711f09ff0f3c3aee5aba3350
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-02-20 11:19:58 +0600

    Done primary setup

---
+++ src/routes/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <p>Error page</p>,
+    children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+    ]
+  },
+]);
+
+export default router;
