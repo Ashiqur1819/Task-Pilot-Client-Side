238c06147a0d242faa963031d3da66b454606e45
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-02-21 16:45:30 +0600

    Done delete functionality for a task

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,35 @@
+import Swal from "sweetalert2";
+import useAxios from "../hooks/useAxios";
 
 
-const TaskCard = ({task}) => {
+const TaskCard = ({task, refetch}) => {
     const { title, description, timestamp, category } = task;
-    console.log(task)
+    const axiosInstance = useAxios()
+    
+  const handleTaskDelete = task => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosInstance.delete(`/tasks/${task?._id}`);
+        if (res.data.deletedCount > 0) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted successfully.",
+              icon: "success",
+            });
+                      refetch();
+        }
+      }
+    });
+  }
+
     return (
       <div className="shadow-xl p-3 mb-6 bg-white">
         <h2 className="text-xl font-semibold text-green-500 mb-2">{title}</h2>
@@ -10,6 +37,14 @@ const TaskCard = ({task}) => {
         <div className="flex items-center justify-between mt-2">
           <p className={`font-medium text-gray-700`}>{category}</p>
           <p className="font-medium text-gray-700">{timestamp.split("T")[0]}</p>
+        </div>
+        <div className="flex items-center gap-3 mt-3">
+          <button className="bg-green-400 text-sm px-3 py-1 text-gray-700 font-medium hover:bg-green-500 cursor-pointer">
+            Update
+          </button>
+          <button onClick={() => handleTaskDelete(task)} className="bg-red-500 text-sm px-3 py-1 text-white font-medium hover:bg-red-600 cursor-pointer">
+            Delete
+          </button>
         </div>
       </div>
     );
--- src/pages/AllTasks/AllTasks.jsx
+++ src/pages/AllTasks/AllTasks.jsx
@@ -8,7 +8,7 @@ import allTaskBannerImage from "../../assets/bg4.jpg"
 
 const AllTasks = () => {
   const axiosInstance = useAxios();
-  const { data: tasks = [] } = useQuery({
+  const { data: tasks = [], refetch } = useQuery({
     queryKey: ["tasks"],
     queryFn: async () => {
       const res = await axiosInstance.get("/tasks");
@@ -36,7 +36,7 @@ const AllTasks = () => {
     >
       {" "}
       <div className="invisible mb-12">dfd</div>
-      <div className="md:max-w-5xl md:mx-auto mx-4 px-4 md:px-6 lg:px-8 py-12  text-gray-700 bg-base-100">
+      <div className="md:max-w-6xl md:mx-auto mx-4 px-4 md:px-6 lg:px-8 py-12  text-gray-700 bg-base-100">
         <h2 className="text-3xl md:text-4xl text-center font-bold">
           All Tasks
         </h2>
@@ -46,7 +46,11 @@ const AllTasks = () => {
             <h2 className="text-2xl mb-3 font-bold">To-Do</h2>
             <div>
               {todoTasks.map((task) => (
-                <TaskCard task={task} key={task._id}></TaskCard>
+                <TaskCard
+                  refetch={refetch}
+                  task={task}
+                  key={task._id}
+                ></TaskCard>
               ))}
             </div>
           </div>
@@ -54,7 +58,11 @@ const AllTasks = () => {
             <h2 className="text-2xl mb-3 font-bold">In-Progress</h2>
             <div>
               {inProgressTasks.map((task) => (
-                <TaskCard task={task} key={task._id}></TaskCard>
+                <TaskCard
+                  refetch={refetch}
+                  task={task}
+                  key={task._id}
+                ></TaskCard>
               ))}
             </div>
           </div>
@@ -62,7 +70,11 @@ const AllTasks = () => {
             <h2 className="text-2xl mb-3 font-bold">Done</h2>
             <div>
               {DoneTasks.map((task) => (
-                <TaskCard task={task} key={task._id}></TaskCard>
+                <TaskCard
+                  refetch={refetch}
+                  task={task}
+                  key={task._id}
+                ></TaskCard>
               ))}
             </div>
           </div>
